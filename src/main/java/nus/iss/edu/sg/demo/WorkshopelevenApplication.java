005f4ff872a091d38d4fe416f8c75c2913f78ad4
MichelleHoZY <hozhiying@outlook.com>
2022-02-28 22:30:03 +0800

    new

---
+++ src/main/java/nus/iss/edu/sg/demo/WorkshopelevenApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.edu.sg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopelevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopelevenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/edu/sg/demo/WorkshopelevenApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.edu.sg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopelevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
